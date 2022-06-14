09af902fb0791dedccb3e278319694302e5e497b
Nicolas Faraz <nicolas.luis.faraz@accenture.com>
2022-06-14 19:53:48 -0300

    feat: add-nock-mock-test

--- test/itemTest.js
+++ test/itemTest.js
@@ -1,19 +1,38 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
+var nock = require('nock');
 
 const app = require('../app');
 const config = require('../config');
 
 chai.should();
 chai.use(chaiHttp);
+const dataItem = require('../mocks/dataMeliItemId.json');
+const dataItemDescription = require('../mocks/dataMeliItemIdDescription.json');
+const dataUser = require('../mocks/dataMeliUser.json');
 
 
 /*probando item */
 describe('get Item:', () => {
+    beforeEach(() => {
+        nock(process.env.BASE_URL)
+            .get('/items/MLA627715476')
+            .reply(200, dataItem);
+        nock(process.env.BASE_URL)
+            .get('/items/MLA627715476/description')
+            .reply(200, dataItemDescription);
+        nock(process.env.BASE_URL)
+            .get('/users/2890353')
+            .reply(200, dataUser);
+    });
+    afterEach(() => {
+        nock.cleanAll();
+    })
+
     it('Should response item 200 OK', (done) => {
         chai.request(app)
-            .get('/api/item/MLA916148161')
+            .get('/api/item/MLA627715476')
             .set({ 'x-auth-token': config.token })
             .end((err, res) => {
                 //console.log(res.body);
@@ -24,7 +43,7 @@ describe('get Item:', () => {
     });
     it('Should response item mock 200 OK', (done) => {
         chai.request(app)
-            .get('/api/item/MLA916148161')
+            .get('/api/item/MLA627715476')
             .set({ 'x-auth-token': config.tokenMock })
             .end((err, res) => {
                 // console.log(res.body);
@@ -34,7 +53,7 @@ describe('get Item:', () => {
     });
     it('Should response item not token', (done) => {
         chai.request(app)
-            .get('/api/item/MLA916148161')
+            .get('/api/item/MLA627715476')
             .end((err, res) => {
                 // console.log(res.body);
                 expect(res).to.have.status(401);
@@ -51,4 +70,5 @@ describe('get Item:', () => {
                 done();
             });
     });
+
 });
--- test/searchTest.js
+++ test/searchTest.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
+var nock = require('nock');
 
 const app = require('../app');
 const config = require('../config');
@@ -8,21 +9,34 @@ const config = require('../config');
 chai.should();
 chai.use(chaiHttp);
 
+const dataSearch = require('../mocks/dataMeliSearch.json');
 
 /*probando search */
 describe('get search:', () => {
+    afterEach(() => {
+        nock.cleanAll();
+    })
+
     it('Should response search 200 OK', (done) => {
+        nock(process.env.BASE_URL)
+            .get('/sites/MLA/search')
+            .query({ q: 'celular', limit: 5, offset: 5 })
+            .reply(200, dataSearch);
         chai.request(app)
             .get('/api/search/MLA?q=celular&limit=5&offset=5')
             .set({ 'x-auth-token': config.token })
             .end((err, res) => {
                 //console.log(res.body);
                 expect(res).to.have.status(200);
-                expect(res.body).to.have.all.keys('paging', 'categories','items');
+                expect(res.body).to.have.all.keys('paging', 'categories', 'items');
                 done();
             });
     });
     it('Should response search 200 OK dont limit', (done) => {
+        nock(process.env.BASE_URL)
+            .get('/sites/MLA/search')
+            .query({ q: 'celular', limit: 50, offset: 5 })
+            .reply(200, dataSearch);
         chai.request(app)
             .get('/api/search/MLA?q=celular&offset=5')
             .set({ 'x-auth-token': config.token })
@@ -33,6 +47,10 @@ describe('get search:', () => {
             });
     });
     it('Should response search 200 OK dont offset', (done) => {
+        nock(process.env.BASE_URL)
+            .get('/sites/MLA/search')
+            .query({ q: 'celular', limit: 5, offset: 0 })
+            .reply(200, dataSearch);
         chai.request(app)
             .get('/api/search/MLA?q=celular&limit=5')
             .set({ 'x-auth-token': config.token })
