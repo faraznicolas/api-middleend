9dd20713effcc2177d13c4099605e188e02ebb7b
Nicolas Faraz <nicolas.luis.faraz@accenture.com>
2022-06-16 10:13:24 -0300

    feat: check

--- routes/item.js
+++ routes/item.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { getItem } = require("../controllers/item");
 const { validarCampos } = require("../middlewares/validar-campos");
 const { validarToken } = require("../middlewares/validar-token");
@@ -8,6 +9,7 @@ const router = Router();
 
 //GET
 router.get('/:id', [
+    check('id', 'Debe ser de tipo string').custom((val) => (typeof val === 'string' && isNaN(val)) ? true : false),
     validarToken,
     validarCampos
 ], getItem);
